5a5817ecaba7cebb5e9bffa146136904e8ffb61e
ahmsay <ahmsay@gmail.com>
2020-11-22 16:30:34 +0300

    handle click test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  state = {
+    name: 'asd'
+  }
+  handle = (e) => {
+    console.log(this)
+  }
   render() {
     return (
       <div className="App">
-        hello
+        <button onClick={ this.handle }>click me</button>
       </div>
-    );
+    )
   }
 }
 

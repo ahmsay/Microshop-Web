97cd697828dc32293efc2308a5ff39e23a812d4a
ahmsay <ahmsay@gmail.com>
2020-11-22 16:48:38 +0300

    service urls are defined

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import React, { Component } from 'react'
 
 class App extends Component {
   state = {
-    name: 'asd'
+    urls: {
+      customers: 'localhost:8081/customers',
+      products: 'localhost:8082/products',
+      payments: 'localhost:8083/payments',
+      orders: 'localhost:8084/orders'
+    }
   }
-  handle = (e) => {
-    console.log(this)
+  fetch = (url) => {
+    console.log(url)
   }
   render() {
     return (
       <div className="App">
-        <button onClick={ this.handle }>click me</button>
+        <button onClick={ () => this.fetch(this.state.urls.customers) }>Customers</button>
+        <br/>
+        <button onClick={ () => this.fetch(this.state.urls.products) }>Products</button>
+        <br/>
+        <button onClick={ () => this.fetch(this.state.urls.payments) }>Payments</button>
+        <br/>
+        <button onClick={ () => this.fetch(this.state.urls.orders) }>Orders</button>
       </div>
     )
   }

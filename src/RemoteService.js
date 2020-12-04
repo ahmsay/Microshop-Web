9b876804c31079510087898c82e550f2277a6f5b
ahmsay <ahmsay@gmail.com>
2020-12-04 14:14:59 +0300

    url encapsulation in remote service

--- src/RemoteService.js
+++ src/RemoteService.js
@@ -1,30 +1,37 @@
 import axios from 'axios'
 
+const urls = {
+  accountService: 'http://localhost:8081',
+  inventoryService: 'http://localhost:8082',
+  paymentService: 'http://localhost:8084',
+  orderService: 'http://localhost:8083'
+}
+
 const RemoteService = {
   getCustomers() {
     return new Promise(resolve => {
-      axios.get('http://localhost:8081/customers').then(response => {
+      axios.get(urls.accountService + '/customers').then(response => {
         resolve(response.data)
       })
     })
   },
   getProducts() {
     return new Promise(resolve => {
-      axios.get('http://localhost:8082/products').then(response => {
+      axios.get(urls.inventoryService + '/products').then(response => {
         resolve(response.data)
       })
     })
   },
   getPayments() {
     return new Promise(resolve => {
-      axios.get('http://localhost:8084/payments').then(response => {
+      axios.get(urls.paymentService + '/payments').then(response => {
         resolve(response.data)
       })
     })
   },
   getOrders() {
     return new Promise(resolve => {
-      axios.get('http://localhost:8083/orders').then(response => {
+      axios.get(urls.orderService + '/orders').then(response => {
         resolve(response.data)
       })
     })

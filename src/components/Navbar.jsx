57a654de16fe936486bf2eb1607e074fcbd482a1
ahmsay <ahmsay@gmail.com>
2020-12-06 13:19:48 +0300

    thats better

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import { Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > * + *': {
+      marginLeft: theme.spacing(2)
+    }
+  }
+}))
 
 const Navbar = () => {
+  const classes = useStyles()
   return (
     <nav>
-      <div>
+      <Typography className={classes.root}>
         <Link to='/'>Dashboard</Link>
         <NavLink to='/customers'>Customers</NavLink>
         <NavLink to='/products'>Products</NavLink>
         <NavLink to='/payments'>Payments</NavLink>
         <NavLink to='/orders'>Orders</NavLink>
-      </div>
+      </Typography>
     </nav>
   )
 }

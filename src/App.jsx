22c3826a0b38cc7734c845e0aec6b445f6f7ab15
ahmsay <ahmsay@gmail.com>
2020-12-06 12:36:05 +0300

    unused stuff is removed from app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Dashboard from './Dashboard'
 import CustomerList from './customer/CustomerList'
 import ProductList from './product/ProductList'
 import PaymentList from './payment/PaymentList'
 import OrderList from './order/OrderList'
-import { AccountRemoteService, InventoryRemoteService, PaymentRemoteService, OrderRemoteService } from './RemoteService'
-import { Container, Button } from '@material-ui/core'
+import { Container } from '@material-ui/core'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
@@ -19,50 +18,22 @@ const theme = createMuiTheme({
   }
 })
 
-class App extends Component {
-  state = {
-    customers: [],
-    products: [],
-    payments: [],
-    orders: []
-  }
-  getCustomers = () => {
-    AccountRemoteService.getCustomers().then(customers => {
-      this.setState({ customers })
-    })
-  }
-  getProducts = () => {
-    InventoryRemoteService.getProducts().then(products => {
-      this.setState({ products })
-    })
-  }
-  getPayments = () => {
-    PaymentRemoteService.getPayments().then(payments => {
-      this.setState({ payments })
-    })
-  }
-  getOrders = () => {
-    OrderRemoteService.getOrders().then(orders => {
-      this.setState({ orders })
-    })
-  }
-  render() {
-    return (
-      <BrowserRouter>
-        <MuiThemeProvider theme={ theme }>
-          <Container>
-            <Switch>
-              <Route exact path='/' component={ Dashboard }/>
-              <Route path='/customers' component={ CustomerList }/>
-              <Route path='/products' component={ ProductList }/>
-              <Route path='/payments' component={ PaymentList }/>
-              <Route path='/orders' component={ OrderList }/>
-            </Switch>
-          </Container>
-        </MuiThemeProvider>
-      </BrowserRouter>
-    )
-  }
+const App = () => {
+  return (
+    <MuiThemeProvider theme={ theme }>
+      <Container>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={ Dashboard }/>
+            <Route path='/customers' component={ CustomerList }/>
+            <Route path='/products' component={ ProductList }/>
+            <Route path='/payments' component={ PaymentList }/>
+            <Route path='/orders' component={ OrderList }/>
+          </Switch>
+        </BrowserRouter>
+      </Container>
+    </MuiThemeProvider>
+  )
 }
 
 export default App

3bfddc7689237611d56825c4cd498087e5c6d02b
ahmsay <ahmsay@gmail.com>
2020-12-07 20:45:29 +0300

    routes are added to appbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { AppBar, Toolbar, Button, Typography, ButtonGroup } from '@material-ui/core'
 
 const Navbar = () => {
+  const pages = [
+    { title: 'Customers', route: '/customers' },
+    { title: 'Products', route: '/products' },
+    { title: 'Payments', route: '/payments' },
+    { title: 'Orders', route: '/orders' }
+  ]
+  const buttons = pages.map(page => {
+    return (
+      <Button key={ page.route } component={ Link } to={ page.route }>
+        <Typography>
+          { page.title }
+        </Typography>
+      </Button>
+    )
+  })
   return (
     <AppBar position="static">
       <Toolbar>
         <ButtonGroup variant="text">
-          test
+          { buttons }
         </ButtonGroup>
       </Toolbar>
     </AppBar>

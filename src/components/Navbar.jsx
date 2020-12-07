40039eb2696537c08c733b853208fb937ec2b5ac
ahmsay <ahmsay@gmail.com>
2020-12-07 20:35:41 +0300

    appbar is added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,16 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import { Typography } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > * + *': {
-      marginLeft: theme.spacing(2)
-    }
-  }
-}))
+import { AppBar, Toolbar, Button, Typography, ButtonGroup } from '@material-ui/core'
 
 const Navbar = () => {
-  const classes = useStyles()
   return (
-    <nav>
-      <Typography className={classes.root}>
-        <Link to='/'>Dashboard</Link>
-        <NavLink to='/customers'>Customers</NavLink>
-        <NavLink to='/products'>Products</NavLink>
-        <NavLink to='/payments'>Payments</NavLink>
-        <NavLink to='/orders'>Orders</NavLink>
-      </Typography>
-    </nav>
+    <AppBar position="static">
+      <Toolbar>
+        <ButtonGroup variant="text">
+          test
+        </ButtonGroup>
+      </Toolbar>
+    </AppBar>
   )
 }
 

0c547cc79a7d41e8451d737b32563028d1eb386e
ahmsay <ahmsay@gmail.com>
2020-12-08 20:51:59 +0300

    table proper implementation is completed

--- src/components/customer/CustomerList.jsx
+++ src/components/customer/CustomerList.jsx
@@ -19,51 +19,33 @@ class CustomerList extends Component {
   render() {
     let tableData = this.state.customers.map(customer => {
       return (
-        <tr key={ customer.id }>
-          <td onClick={ () => this.getRecord(customer.id) }>{ customer.id }</td>
-          <td>{ customer.name }</td>
-        </tr>
+        <TableRow key={customer.id}>
+          <TableCell component="th" scope="row">
+            {customer.id}
+          </TableCell>
+          <TableCell align="right">{customer.name}</TableCell>
+        </TableRow>
       )
     })
     let table = (
-      <div>
-        <h3>Customers</h3>
-        <table id="customerTable">
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Name</th>
-            </tr>
-          </thead>
-          <tbody>
+      <TableContainer component={ Paper }>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell align="right">Name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
             { tableData }
-          </tbody>
-        </table>
-      </div>
+          </TableBody>
+        </Table>
+      </TableContainer>
     )
     return (
       <div>
-        {/* tableData.length !== 0 ? table : null */}
-        <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Id</TableCell>
-                <TableCell align="right">Name</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {this.state.customers.map((customer) => (
-                <TableRow key={customer.id}>
-                  <TableCell component="th" scope="row">
-                    {customer.id}
-                  </TableCell>
-                  <TableCell align="right">{customer.name}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <h3>Customers</h3>
+        { tableData.length !== 0 ? table : null }
       </div>
     )
   }

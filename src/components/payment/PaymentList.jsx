c8c585ea9dc7722396d749c4a8f28305cb6d9763
ahmsay <ahmsay@gmail.com>
2020-12-14 21:31:56 +0300

    payments are rendered as list

--- src/components/payment/PaymentList.jsx
+++ src/components/payment/PaymentList.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import { PaymentRemoteService } from '../../RemoteService'
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core'
+import { List, ListItem, ListItemText } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
 
 const useStyles = (theme) => ({
-  table: theme.table,
   title: theme.title,
-  tableCell: theme.tableCell
+  list: theme.list
 })
 
 class PaymentList extends Component {
@@ -25,35 +24,22 @@ class PaymentList extends Component {
   }
   render() {
     const { classes } = this.props
-    let tableData = this.state.payments.map(payment => {
+    let listData = this.state.payments.map(payment => {
       return (
-        <TableRow key={ payment.id } onClick={ () => this.getRecord(payment.id) }>
-          <TableCell className={ classes.tableCell } component="th" scope="row">{ payment.id }</TableCell>
-          <TableCell className={ classes.tableCell } align="right">{ payment.totalCharge }</TableCell>
-          <TableCell className={ classes.tableCell } align="right">{ payment.customerId }</TableCell>
-        </TableRow>
+        <ListItem key={ payment.id } onClick={ () => this.getRecord(payment.id) } button>
+          <ListItemText primary={ payment.id } secondary={ '$' + payment.totalCharge }/>
+        </ListItem>
       )
     })
-    let table = (
-      <TableContainer className={ classes.table } component={ Paper }>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell className={ classes.tableCell }>Id</TableCell>
-              <TableCell className={ classes.tableCell } align="right">Total Charge</TableCell>
-              <TableCell className={ classes.tableCell } align="right">Customer Id</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            { tableData }
-          </TableBody>
-        </Table>
-      </TableContainer>
+    let list = (
+      <List className={ classes.list }>
+        { listData }
+      </List>
     )
     return (
       <div>
         <h3 className={ classes.title }>Payments</h3>
-        { tableData.length !== 0 ? table : null }
+        { listData.length === 0 ? null : list }
       </div>
     )
   }

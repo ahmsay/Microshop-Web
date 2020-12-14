8cc44318fa9bbdb4e973dcc2cfd57540240af2e2
ahmsay <ahmsay@gmail.com>
2020-12-14 21:26:36 +0300

    products are rendered as list

--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import { InventoryRemoteService } from '../../RemoteService'
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core'
+import { List, ListItem, ListItemText } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
 
 const useStyles = (theme) => ({
-  table: theme.table,
   title: theme.title,
-  tableCell: theme.tableCell
+  list: theme.list
 })
 
 class ProductList extends Component {
@@ -25,39 +24,22 @@ class ProductList extends Component {
   }
   render() {
     const { classes } = this.props
-    let tableData = this.state.products.map(product => {
+    let listData = this.state.products.map(product => {
       return (
-        <TableRow key={ product.id } onClick={ () => this.getRecord(product.id) }>
-          <TableCell className={ classes.tableCell } component="th" scope="row">{ product.id }</TableCell>
-          <TableCell className={ classes.tableCell } align="right">{ product.name }</TableCell>
-          <TableCell className={ classes.tableCell } align="right">{ product.price }</TableCell>
-          <TableCell className={ classes.tableCell } align="right">{ product.category }</TableCell>
-          <TableCell className={ classes.tableCell } align="right">{ product.paymentId }</TableCell>
-        </TableRow>
+        <ListItem key={ product.id } onClick={ () => this.getRecord(product.id) } button>
+          <ListItemText primary={ product.name } secondary={ '$' + product.price }/>
+        </ListItem>
       )
     })
-    let table = (
-      <TableContainer className={ classes.table } component={ Paper }>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell className={ classes.tableCell }>Id</TableCell>
-              <TableCell className={ classes.tableCell } align="right">Name</TableCell>
-              <TableCell className={ classes.tableCell } align="right">Price</TableCell>
-              <TableCell className={ classes.tableCell } align="right">Category</TableCell>
-              <TableCell className={ classes.tableCell } align="right">Payment Id</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            { tableData }
-          </TableBody>
-        </Table>
-      </TableContainer>
+    const list = (
+      <List className={ classes.list }>
+        { listData }
+      </List>
     )
     return (
       <div>
         <h3 className={ classes.title }>Products</h3>
-        { tableData.length !== 0 ? table : null }
+        { listData.length === 0 ? null : list }
       </div>
     )
   }

475f2f9aa80738ab3e15999b05184ec08831b81a
ahmsay <ahmsay@gmail.com>
2020-12-16 20:14:57 +0300

    simple dialog implementation

--- src/components/customer/CustomerList.jsx
+++ src/components/customer/CustomerList.jsx
@@ -1,16 +1,38 @@
 import React, { Component } from 'react'
 import { AccountRemoteService } from '../../RemoteService'
-import { List, ListItem, ListItemText } from '@material-ui/core'
+import { List, ListItem, ListItemText, Dialog, DialogTitle, Button } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
+import PropTypes from 'prop-types'
 
 const useStyles = (theme) => ({
   title: theme.title,
   list: theme.list
 })
 
+function SimpleDialog(props) {
+  const { onClose, open } = props
+
+  const handleClose = () => {
+    onClose()
+  }
+
+  return (
+    <Dialog onClose={ handleClose } aria-labelledby="simple-dialog-title" open={open}>
+      <DialogTitle>Set backup account</DialogTitle>
+      test
+    </Dialog>
+  )
+}
+
+SimpleDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired
+}
+
 class CustomerList extends Component {
   state = {
-    customers: []
+    customers: [],
+    open: false
   }
   componentDidMount() {
     AccountRemoteService.getCustomers().then(customers => {
@@ -24,6 +46,13 @@ class CustomerList extends Component {
   }
   render() {
     const { classes } = this.props
+    const handleClickOpen = () => {
+      console.log(1)
+      this.setState({ open: true })
+    }
+    const handleClose = () => {
+      this.setState({ open: false })
+    }
     const listData = this.state.customers.map(customer => {
       return (
         <ListItem key={ customer.id } onClick={ () => this.getRecord(customer.id) } button>
@@ -40,6 +69,10 @@ class CustomerList extends Component {
       <div>
         <h3 className={ classes.title }>Customers</h3>
         { listData.length === 0 ? null : list }
+        <Button variant="outlined" color="secondary" onClick={ handleClickOpen }>
+          Open simple dialog
+        </Button>
+        <SimpleDialog open={this.state.open} onClose={ handleClose } />
       </div>
     )
   }

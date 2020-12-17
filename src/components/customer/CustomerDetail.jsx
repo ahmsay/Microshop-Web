a0816f43c3ba49a684ca31fef445023a0a5c5659
ahmsay <ahmsay@gmail.com>
2020-12-17 20:23:09 +0300

    customer detail is shown in card

--- src/components/customer/CustomerDetail.jsx
+++ src/components/customer/CustomerDetail.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
-import { Dialog, DialogTitle } from '@material-ui/core'
+import { Dialog, Card, CardContent, CardActions, Button, Typography } from '@material-ui/core'
 
 const CustomerDetail = ({ open, customer, onClose }) => {
   const handleClose = () => { onClose() }
   return (
-    <Dialog onClose={ handleClose } aria-labelledby="simple-dialog-title" open={open}>
-      <DialogTitle>Customer Detail</DialogTitle>
-      <div>
-        { customer.id }
-      </div>
-      <div>
-        { customer.name }
-      </div>
-      <div>
-        { customer.paymentList.length }
-      </div>
-      <div>
-        { customer.orderList.length }
-      </div>
+    <Dialog onClose={ handleClose } open={open}>
+      <Card>
+        <CardContent>
+          <Typography gutterBottom variant="h6">
+            Customer Detail
+          </Typography>
+          <Typography variant="body2">
+            Id: { customer.id }
+          </Typography>
+          <Typography variant="body2">
+            Name: { customer.name }
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Close</Button>
+        </CardActions>
+      </Card>
     </Dialog>
   )
 }

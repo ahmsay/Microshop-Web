148767644b247fa8a2dffa3d53fb8ab6102bbad4
ahmsay <ahmsay@gmail.com>
2020-12-18 20:08:10 +0300

    refactor

--- src/components/CustomTable.jsx
+++ src/components/CustomTable.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
-const CustomTable = ({ }) => {
+const CustomTable = () => {
   const fieldNames = ['Id', 'Name']
   const data = [{id: 1, name: 'Zorkov'}, {id: 2, name: 'Nihilus'}]
   const tableHead = fieldNames.map(field => {
-    <p>{ field } </p>
+    return (
+      <span key={ field }>{ field } </span>
+    )
   })
   const tableBody = data.map(record => {
     return (
-      <div>
-        <p>{ record.id }</p>
-        <p>{ record.name }</p>
+      <div key={ record.id }>
+        <span>{ record.id } </span>
+        <span>{ record.name } </span>
       </div>
     )
   })

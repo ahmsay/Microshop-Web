151e87213d17165ced186add574039447853b81e
ahmsay <ahmsay@gmail.com>
2020-12-18 20:00:55 +0300

    custom table first try

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
 const Table = ({ }) => {
-  <div>asd</div>
+  const fieldNames = ['Id', 'Name']
+  const data = [{id: 1, name: 'Zorkov'}, {id: 2, name: 'Nihilus'}]
+  const tableHead = fieldNames.map(field => {
+    <p>{ field } </p>
+  })
+  const tableBody = data.map(record => {
+    return (
+      <div>
+        <p>{ record.id }</p>
+        <p>{ record.name }</p>
+      </div>
+    )
+  })
+  return (
+    <div>
+      { tableHead }
+      { tableBody }
+    </div>
+  )
 }
 
 export default Table

7c644e3b6320fc1d196601478a8257255cca9e3d
ahmsay <ahmsay@gmail.com>
2020-12-19 12:49:36 +0300

    datagrid is removed. useless af

--- src/components/customer/CustomerDetail.jsx
+++ src/components/customer/CustomerDetail.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Dialog, Card, CardContent, CardActions, Button, Typography } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
-import { DataGrid } from '@material-ui/data-grid'
 
 const useStyles = (theme) => ({
   card: theme.card
@@ -15,7 +14,7 @@ const CustomerDetail = ({ open, customer, toggle, classes }) => {
   ]
   return (
     <Dialog onClose={ closeDialog } open={ open }>
-      <Card style={{ width: 500, height: 500 }} className={ classes.card }>
+      <Card className={ classes.card }>
         <CardContent>
           <Typography gutterBottom variant="h6">
             Customer Detail
@@ -30,7 +29,6 @@ const CustomerDetail = ({ open, customer, toggle, classes }) => {
         <CardActions>
           <Button size="small" onClick={ closeDialog }>Close</Button>
         </CardActions>
-        <DataGrid rows={ customer.paymentList } columns={ paymentColumns }/>
       </Card>
     </Dialog>
   )

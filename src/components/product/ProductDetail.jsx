ce247e9059be53d8e10d7c670a662a8ff9e8cbe8
ahmsay <ahmsay@gmail.com>
2020-12-19 14:33:57 +0300

    product detail page is finished

--- src/components/product/ProductDetail.jsx
+++ src/components/product/ProductDetail.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Dialog, Card, CardContent, CardActions, Button, Typography, TableContainer, Table, TableHead, TableBody, TableCell, TableRow } from '@material-ui/core'
+import { Dialog, Card, CardContent, CardActions, Button, Typography } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
 
 const useStyles = (theme) => ({
@@ -10,9 +10,44 @@ const useStyles = (theme) => ({
 
 const ProductDetail = ({ open, product, toggle, classes }) => {
   const closeDialog = () => { toggle() }
+  const paymentInfo = (
+    <div>
+      <Typography gutterBottom variant="h6">
+        <b>Payment Information</b>
+      </Typography>
+      <Typography variant="body1">
+        Id: { product.payment.id }
+      </Typography>
+      <Typography variant="body1">
+        Total Charge: { product.payment.totalCharge }
+      </Typography>
+    </div>
+  )
   return (
     <Dialog onClose={ closeDialog } open={ open }>
-      <div>product detail</div>
+      <Card className={ classes.card }>
+        <CardContent>
+          <Typography gutterBottom variant="h6">
+            <b>Product Detail</b>
+          </Typography>
+          <Typography variant="body1">
+            Id: { product.id }
+          </Typography>
+          <Typography variant="body1">
+            Name: { product.name }
+          </Typography>
+          <Typography variant="body1">
+            Category: { product.category }
+          </Typography>
+          <Typography variant="body1">
+            Price: { product.price }
+          </Typography>
+          { paymentInfo }
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={ closeDialog }>Close</Button>
+        </CardActions>
+      </Card>
     </Dialog>
   )
 }

282a5e064af07ff9a32a4c21bba891971cebb3f7
ahmsay <ahmsay@gmail.com>
2021-02-02 19:45:57 +0300

    indentation fix

--- src/components/customer/AddCustomer.jsx
+++ src/components/customer/AddCustomer.jsx
@@ -1,28 +1,28 @@
 import React, { Component } from 'react'
 
 class AddCustomer extends Component {
-    state = {
-        name: ''
-    }
-    handleChange = (e) => {
-        this.setState({
-            [e.target.id]: e.target.value
-        })
-    }
-    handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(this.state)
-    }
-    render() {
-        return (
-            <form onSubmit={ this.handleSubmit }>
-                <h4>Add New Customer</h4>
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name" onChange={ this.handleChange }/>
-                <button>Add</button>
-            </form>
-        )
-    }
+  state = {
+      name: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+  render() {
+    return (
+      <form onSubmit={ this.handleSubmit }>
+        <h4>Add New Customer</h4>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" onChange={ this.handleChange }/>
+        <button>Add</button>
+      </form>
+    )
+  }
 }
 
 export default AddCustomer
